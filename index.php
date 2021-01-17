d7a8f7e71e1fed225dabae4a0d6cc2625741393b
sayyid129 <sayyid.jamil85@gmail.com>
2021-01-17 15:34:55 +0700

    menambakan data

--- index.php
+++ index.php
@@ -10,5 +10,10 @@
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quam laudantium quis expedita repellat dolore nam voluptatum consectetur optio? Quas beatae blanditiis nemo praesentium exercitationem optio obcaecati repellat, repudiandae natus.</p>
 
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+
 </body>
 </html>
